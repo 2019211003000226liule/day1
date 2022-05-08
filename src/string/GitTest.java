10b94aa760c6de91f6d64b96473780dd075cd907
2019211003000226liule <1478108793@qq.com>
2022-05-08 13:57:38 +0800

    first commit

---
+++ src/string/GitTest.java
@@ -0,0 +1,15 @@
+package string;
+
+/**
+ * ClassName:GitTest
+ * Package:string
+ * Description:
+ *
+ * @Date:2022/5/8 13:56
+ * Author:1478108793@qq.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
