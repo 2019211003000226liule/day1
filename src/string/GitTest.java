b56ac141653127d18997d55e13d1a6074d2739c0
2019211003000226liule <1478108793@qq.com>
2022-05-08 14:42:28 +0800

    ssh commit test

--- src/string/GitTest.java
+++ src/string/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot fix");
         System.out.println("hot fix1");
+        System.out.println("ssh git test");
     }
 }
